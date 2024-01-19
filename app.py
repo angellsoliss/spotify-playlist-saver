b12655d59bc9734dfec1c8708cad9c93d39e925a
Angel <chanfle@boyd-tech.com>
2024-01-19 09:17:00 -0600

    made site public, disabled debug

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def create_spotify_oauth():
                         scope='user-library-read playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
